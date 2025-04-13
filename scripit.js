67875c25c4a4d2695049933b42b91bb1253df7f9
brennogbi <brenoguanambi@hotmail.com>
2025-04-13 02:24:32 -0300

    1comit produto nike

---
+++ scripit.js
@@ -0,0 +1,37 @@
+const circles = document.querySelectorAll('.circle'); // todas as bolinhas de cores
+const imageArea = document.querySelector('.image-area'); // apenas um elemento
+const tenisImg = document.getElementById('tenis-img'); // imagem do tênis
+const botao = document.querySelector('.bnt')
+const contadordeproduto = document.getElementById('contador-carrinho')
+
+let carrinho = 0
+
+
+
+
+botao.addEventListener('click',() =>{
+
+    carrinho++
+    contadordeproduto.innerHTML= carrinho
+  alert('produto adicionado ao carrinh')  
+
+
+})
+
+
+
+
+
+circles.forEach(circle => {
+    circle.addEventListener('click', () => {
+        const color = circle.getAttribute('data-color'); // ex: "#ff0000"
+        const imagem = circle.getAttribute('data-img'); // ex: "tenis-vermelho.png"
+
+        // muda a cor de fundo da área de imagem
+        imageArea.style.backgroundColor = color;
+
+        // muda a imagem do tênis
+
+        tenisImg.src = imagem;
+    });
+});
